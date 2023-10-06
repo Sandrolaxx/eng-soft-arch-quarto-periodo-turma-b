6d2cb982fc270960644eb2783cab99b6a542711c
jvsantos27 <jvsantos4@minha.fag.edu.br>
2023-10-05 22:03:05 -0300

    Trabalho do segundo bimestre

---
+++ Joao_Santos/segundobimestre/recarga-api/src/main/java/com/fag/domain/entities/OperatorBO.java
@@ -0,0 +1,5 @@
+package com.fag.domain.entities;
+
+public class OperatorBO {
+    // Criar os atributos de acordo com o site
+}
---
+++ Joao_Santos/segundobimestre/recarga-api/src/main/java/com/fag/domain/entities/ProductBO.java
@@ -0,0 +1,5 @@
+package com.fag.domain.entities;
+
+public class ProductBO {
+    // Criar os atributos de acordo com o site
+}
