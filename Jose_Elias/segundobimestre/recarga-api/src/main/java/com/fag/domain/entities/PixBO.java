71120ec8cb17862667c14fbe25e3cdc83f9673a3
joseem <molinetijose@gmail.com>
2023-11-22 17:01:50 -0300

    Update 22/11/2023

--- Jose_Elias/segundobimestre/recarga-api/src/main/java/com/fag/domain/entities/PixBO.java
+++ Jose_Elias/segundobimestre/recarga-api/src/main/java/com/fag/domain/entities/PixBO.java
@@ -1,3 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fag.domain.entities;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ *
+ * @author joseem
+ */
 public class PixBO {
     private UUID id;
     private BigDecimal amount;
@@ -6,6 +20,6 @@ public class PixBO {
     private String emv;
     private String base64;
     private LocalDateTime expiration;
-
+    
     
 }
--- Jose_Elias/segundobimestre/recarga-api/src/main/java/com/fag/domain/mappers/PixMapper.java
+++ Jose_Elias/segundobimestre/recarga-api/src/main/java/com/fag/domain/mappers/PixMapper.java
@@ -1,5 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fag.domain.mappers;
+
+import com.fag.domain.dto.PixDTO;
+import com.fag.domain.entities.PixBO;
+
+/**
+ *
+ * @author joseem
+ */
 public class PixMapper {
     public static PixDTO toDTO(PixBO bo){
         PixDTO dto = new PixDTO();
+        return null;
     }
 }
