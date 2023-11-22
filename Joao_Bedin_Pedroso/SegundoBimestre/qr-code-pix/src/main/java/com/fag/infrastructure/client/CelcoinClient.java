450771a259cf4cb03b97d78191eaa5048b70a62c
JoaoHenriqueBedin <jhbpdayko@minha.fag.edu.br>
2023-11-22 19:50:32 -0300

    Commitando projeto

--- Joao_Bedin_Pedroso/SegundoBimestre/qr-code-pix/src/main/java/com/fag/infrastructure/client/CelcoinClient.java
+++
@@ -1,8 +0,0 @@
-package com.fag.infrastructure.client;
-
-import com.fag.application.dtos.QRCodeRequest;
-
-public interface CelcoinClient {
-    String authenticate();
-    String generatePixQRCode(QRCodeRequest request, String token);
-}
